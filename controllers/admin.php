d8bb963fd6951f2abbfde05f3088166c5b0fe826
nafirilhaq <52308269+nafirilhaq@users.noreply.github.com>
2019-06-29 12:57:45 +0700

    Update admin.php

--- controllers/admin.php
+++ controllers/admin.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Adminquhh extends CI_Controller {
+class Admin extends CI_Controller {
 
 	function __construct() {
         parent::__construct();
